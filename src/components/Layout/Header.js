ab82ad03e9afb5cc75acd099dbe047f526e204ba
Priyanshu Kumar <priyanshu.kr81@gmail.com>
2021-10-31 16:31:26 +0530

    HeaderCartButton

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,4 +1,5 @@
 import {Fragment} from "react";
+import HeaderCartButton from "./HeaderCartButton";
 import mealsImage from '../../assests/meals.jpg'
 import classes from './Header.module.css'
 const Header=props=>{
@@ -6,7 +7,7 @@ const Header=props=>{
         <Fragment>
             <header className={classes.header}>
                 <h1>ReactMeals</h1>
-                <button>Cart</button>
+                <HeaderCartButton/>
             </header>
             <div className={classes['main-image']}>
                 <img src={mealsImage} alt="A Table full of delicious foods"/>
